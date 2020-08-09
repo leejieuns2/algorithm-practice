9a3779599690e6aa54254f73974d1197f2d595cd
leejieuns2 <2jjingni@gmail.com>
2020-08-10 00:51:26 +0900

    add : simulation

---
+++ AlgorithmPractice/src/simulation/Main_3006.java
@@ -0,0 +1,16 @@
+package simulation;
+
+import java.util.Scanner;
+
+/* �˰��� ���͵� - Simulation Algorithm
+ * ���� 3006��.  
+ * https://www.acmicpc.net/problem/3006 */
+
+public class Main_3006 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+	}
+}
