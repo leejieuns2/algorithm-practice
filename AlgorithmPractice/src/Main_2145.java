cee7c5dce04221c31bb23e5ac5c852c47ce69655
leejieuns2 <2jjingni@gmail.com>
2021-02-26 23:59:12 +0900

    fix boj 2145-숫자놀이 (refactoring)

--- AlgorithmPractice/src/Main_2145.java
+++ AlgorithmPractice/src/Main_2145.java
@@ -13,7 +13,6 @@ public class Main_2145 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> list = new ArrayList<Integer>();
 
         while (true) {
             // 입력 받기
@@ -27,17 +26,12 @@ public class Main_2145 {
             String[] num = tmp.split("");
 
             // 계산 하기
-            testNum(list, num);
-        }
-
-        // 결과 출력
-        for(int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+            testNum(num);
         }
     }
 
     // 재귀함수 형식으로 구현
-    static void testNum(ArrayList<Integer> list, String[] num) {
+    static void testNum(String[] num) {
         int rslt = 0;
         for (int i = 0; i < num.length; i++) {
             rslt += Integer.parseInt(num[i]);
@@ -46,10 +40,10 @@ public class Main_2145 {
         // 10으로 나눠서 몫이 0이면 한 자릿수라고 판단함.
         if((rslt / 10) == 0) {
             // 결과가 나왔으므로 결과 list에 넣어주기
-            list.add(rslt);
+            System.out.println(rslt);
         } else {
             // 아직 한 자릿수가 되지 않았을 경우 재귀 호출.
-            testNum(list, String.valueOf(rslt).split(""));
+            testNum(String.valueOf(rslt).split(""));
         }
     }
 }
