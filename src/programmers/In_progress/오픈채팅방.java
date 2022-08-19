8fae2eeb7f703ddf45a1d198ca85ccb21b168da3
leejieuns2 <2jjingni@gmail.com>
2022-08-20 01:31:23 +0900

    fix 오픈채팅방 for class ver.

--- src/programmers/In_progress/오픈채팅방.java
+++ src/programmers/In_progress/오픈채팅방.java
@@ -3,6 +3,7 @@ package programmers.In_progress;
 import java.util.*;
 
 public class 오픈채팅방 {
+
     public static void main(String[] args) {
         String[] str1 = {"Enter uid1234 Muzi", "Enter uid4567 Prodo",
                 "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"};
@@ -15,7 +16,93 @@ public class 오픈채팅방 {
     // record 배열 쪼개기 (행동, 유저 아이디, 닉네임 / 공백 기준으로)
     // 유저 아디이와 닉네임은 알파벳 대문자, 소문자를 구별함.
 
+    private static class User {
+        String id;
+        String nickname;
+
+        User(String id, String nickname) {
+            this.id = id;
+            this.nickname = nickname;
+        }
+    }
+
+    private static class Result {
+        String userId;
+        String action;
+
+        Result(String userId, String action) {
+            this.userId = userId;
+            this.action = action;
+        }
+    }
+
     public static String[] solution(String[] record) {
+        // ID, Nickname 저장할 Hashmap
+        HashMap<String, User> userMap = new HashMap<>();
+        ArrayList<Result> rsltList = new ArrayList<>();
+        for(String str : record) {
+            String[] split_str = str.split(" ");
+
+            // split_str[0] : 행동, split_str[1] : ID , split_str[2] : Nickname
+            switch (split_str[0]) {
+                case "Enter" :
+                    User newUser = new User(split_str[1], split_str[2]);
+                    userMap.put(newUser.id, newUser);
+                    rsltList.add(new Result(newUser.id, split_str[0]));
+                    break;
+                case "Leave" :
+                    User findUser = userMap.get(split_str[1]);
+                    rsltList.add(new Result(findUser.id, split_str[0]));
+                    break;
+                case "Change" :
+                    User changeUser = userMap.get(split_str[1]);
+                    changeUser.nickname = split_str[2];
+                    break;
+            }
+
+        }
+
+        String[] answer = new String[rsltList.size()];
+        int idx = 0;
+        for(Result rslt : rsltList) {
+            User rsltUser = userMap.get(rslt.userId);
+            switch (rslt.action) {
+                case "Enter" :
+                    answer[idx++] = getEnterStr(rsltUser.nickname);
+                    break;
+                case "Leave" :
+                    answer[idx++] = getLeaveStr(rsltUser.nickname);
+                    break;
+            }
+        }
+        return answer;
+    }
+
+    public static String getEnterStr(String nickname) {
+        return nickname + "님이 들어왔습니다.";
+    }
+
+    public static String getLeaveStr(String nickname) {
+        return nickname + "님이 나갔습니다.";
+    }
+
+    /*
+    * 수정 전 코드
+    * */
+
+    public static String addRslt (String id, String action) {
+        return id + " " + action;
+    }
+
+    public static String getEnterStr_before(HashMap<String, String> user, String id) {
+        return user.get(id) + "님이 들어왔습니다.";
+    }
+
+    public static String getLeaveStr_before(HashMap<String, String> user, String id) {
+        return user.get(id) + "님이 나갔습니다.";
+    }
+
+    public static String[] solution_before(String[] record) {
         // ID, Nickname 저장할 Hashmap
         HashMap<String, String> user = new HashMap<>();
         ArrayList<String> rslt = new ArrayList<>();
@@ -44,23 +131,13 @@ public class 오픈채팅방 {
             String[] str = rslt.get(i).split(" ");
             switch (str[1]) {
                 case "Enter" :
-                    answer[i] = getEnterStr(user, str[0]);
+                    answer[i] = getEnterStr_before(user, str[0]);
                     break;
                 case "Leave" :
-                    answer[i] = getLeaveStr(user, str[0]);
+                    answer[i] = getLeaveStr_before(user, str[0]);
                     break;
             }
         }
         return answer;
-    }
-    public static String addRslt (String id, String action) {
-        return id + " " + action;
-    }
-    public static String getEnterStr(HashMap<String, String> user, String id) {
-        return user.get(id) + "님이 들어왔습니다.";
-    }
-
-    public static String getLeaveStr(HashMap<String, String> user, String id) {
-        return user.get(id) + "님이 나갔습니다.";
     }
 }
