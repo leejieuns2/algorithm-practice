60a7eb9d4b9ca1ef2f37536df48d04195a5ace8f
leejieuns2 <2jjingni@gmail.com>
2022-12-04 01:13:20 +0900

    add programmers level 2 - 짝지어 제거하기_final

--- src/programmers/level2/짝지어_제거하기.java
+++ src/programmers/level2/짝지어_제거하기.java
@@ -1,6 +1,6 @@
-package programmers.In_progress;
+package programmers.level2;
 
-import java.util.Arrays;
+import java.util.Stack;
 
 public class 짝지어_제거하기 {
 
@@ -9,8 +9,34 @@ public class 짝지어_제거하기 {
         System.out.println("result 2 : " + solution("cdcd"));
     }
 
-    public static int solution(String s)
-    {
+    public static int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            stack.add(s.charAt(i));
+            // 같은 소문자로 이루어져 있지 않을 때까지 stack에서 제거
+            while(true) {
+                if (stack.size() >= 2) {
+                    char tmp1 = stack.pop();
+                    char tmp2 = stack.pop();
+                    if (tmp1 != tmp2) {
+                        // 넣을때는 뽑은 순서 반대로 넣기
+                        stack.add(tmp2);
+                        stack.add(tmp1);
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int solution_30(String s) {
         int answer = 0;
         int idx = 0;
         while (true) {
